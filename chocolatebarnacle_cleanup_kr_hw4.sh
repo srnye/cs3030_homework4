38540e5a55da76a0ff4afc7bf2fdd3e53d916da9
Kyle Richetti <Kylerichetti@mail.weber.edu>
2016-10-11 13:16:46 -0600

    Added blank files for my task - Kyle R

---
+++ chocolatebarnacle_cleanup_kr_hw4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: chocolatebarnacle_cleanup_kr_hw4.sh
+# 
+#         USAGE: ./chocolatebarnacle_cleanup_kr_hw4.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Kyle Richetti (), kylerichetti@mail.weber.edu
+#  ORGANIZATION: WSU
+#       CREATED: 10/11/2016 13:14
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+
+
+
+exit 0
+
---
+++ chocolatebarnacle_filter_kr_hw4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: chocolatebarnacle_filter_kr_hw4.sh
+# 
+#         USAGE: ./chocolatebarnacle_filter_kr_hw4.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Kyle Richetti (), kylerichetti@mail.weber.edu
+#  ORGANIZATION: WSU
+#       CREATED: 10/11/2016 13:12
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+
+
+exit 0
+
