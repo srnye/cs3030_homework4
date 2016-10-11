51e0e5d5795525027d2f47484bc7240c877dbc64
Steven R. Nye <stevennye@mail.weber.edu>
2016-10-11 12:37:22 -0600

    Initialized wrapper file

---
+++ chocolatebarnacle_srn_hw4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: chocolatebarnacle_srn_hw4.sh
+# 
+#         USAGE: ./chocolatebarnacle_srn_hw4.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Steven R. Nye (), stevennye@mail.weber.edu
+#  ORGANIZATION: Weber State University
+#       CREATED: 10/11/2016 12:36
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
